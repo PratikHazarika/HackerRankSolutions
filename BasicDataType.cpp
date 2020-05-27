c106ba6def43fea739526624f92ac8292d398853
PratikHazarika <automation.qa.sel@gmail.com>
2020-05-27 19:13:40 +0530

    add

---
+++ BasicDataType.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cstdio>
+
+using namespace std;
+
+int main() {
+    // Complete the code.
+    int x;
+    long y;
+    char z;
+    float w;
+    double t;
+
+    scanf("%d %ld %c %f %lf" , &x, &y, &z, &w, &t);
+    printf("%d\n%ld\n%c\n%f\n%lf" , x, y, z, w, t);
+    return 0;
+}
--- InputAndOutput.cpp
+++ InputAndOutput.cpp
@@ -3,12 +3,10 @@
 #include <vector>
 #include <iostream>
 #include <algorithm>
-
 using namespace std;
 
 
-int main() 
-{
+int main() {
     /* Enter your code here. Read input from STDIN. Print output to STDOUT */ 
     int a{0}, b{0}, c{0};
     cin >> a >> b >> c;
